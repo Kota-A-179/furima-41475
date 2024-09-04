ec7ad8b662f9b09aed6a428b5ba217c904f79d58
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-04 18:16:50 +0900

    バリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :content, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :expense_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999}
+
+  belongs_to :user
 end
