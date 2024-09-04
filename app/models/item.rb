6f7e4dacd69e0b790dc12fdbdbcf30216147f571
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-04 21:55:38 +0900

    Itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :expense
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
   belongs_to :user
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :content, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :expense_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_date_id, presence: true
-  validates :price, presence: true, numericality: { in: 300..9999999}
+  validates :image, presence: { message: "を入力してください" }
+  validates :name, presence: { message: "を入力してください" }, length: { maximum: 40, message: "は40字以内で入力してください" }
+  validates :content, presence: { message: "を入力してください" }, length: { maximum: 1000, message: "は1000字以内で入力してください" }
+  validates :category_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
+  validates :condition_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
+  validates :expense_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
+  validates :prefecture_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
+  validates :shipping_date_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
+  validates :price, presence: { message: "を入力してください" }, numericality: { in: 300..9999999, message: "は300円以上、999万999円以下で設定してください"}
   
 
 end
