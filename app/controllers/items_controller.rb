96a01dce6f81deab29cc25cb96f2912b8c2ee6ea
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-05 21:01:42 +0900

    商品情報の表示の設定・リンクの表示設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show 
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :expense_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
