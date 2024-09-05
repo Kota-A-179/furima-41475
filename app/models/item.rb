690cf0fb5a89013a86a3d5d1421d4086d7f37d36
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-05 16:01:26 +0900

    バリデーション・エラーメッセージの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, presence: { message: "を入力してください" }
+  validates :image, presence: { message: "を挿入してください" }
   validates :name, presence: { message: "を入力してください" }, length: { maximum: 40, message: "は40字以内で入力してください" }
   validates :content, presence: { message: "を入力してください" }, length: { maximum: 1000, message: "は1000字以内で入力してください" }
-  validates :category_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
-  validates :condition_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
-  validates :expense_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
-  validates :prefecture_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
-  validates :shipping_date_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を入力してください" }
-  validates :price, presence: { message: "を入力してください" }, numericality: { in: 300..9999999, message: "は300円以上、999万999円以下で設定してください"}
-  
+  validates :category_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を選択してください" }
+  validates :condition_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を選択してください" }
+  validates :expense_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を選択してください" }
+  validates :prefecture_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を選択してください" }
+  validates :shipping_date_id, presence: { message: "を入力してください" }, numericality: { other_than: 1, message: "を選択してください" }
+  validates :price, presence: { message: "を入力してください" }
+  validates :price, numericality: { in: 300..9999999, message: "は半角数字を用いて、300円～999万999円の範囲で設定してください"}, if: -> { price.present? }
 
 end
