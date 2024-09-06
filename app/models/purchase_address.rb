efc75715f1c12c50e20ff085eddb996f0ba1e893
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-06 22:13:02 +0900

    Formオブジェクトの保存機能の設定・バリデーションの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,11 +5,23 @@ class PurchaseAddress
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は半角数字でハイフン(-)を含めて入力してください" }
+    validates :post_code
     validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "は半角数字でハイフン(-)を含めず入力してください" }
-    validates :purchase_id
+    validates :phone_number
+  end
+
+  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は半角数字でハイフン(-)を含めて入力してください" }, if: -> { post_code.present? }
+  validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "は半角数字でハイフン(-)を含めず入力してください" }, if: -> { phone_number.present? }
+
+  def save
+    if valid?
+      purchase = Purchase.create(item_id: item_id, user_id: user_id)
+      Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, phone_number: phone_number, purchase_id: purchase.id)
+      true
+    else
+      false
+    end
   end
 end
