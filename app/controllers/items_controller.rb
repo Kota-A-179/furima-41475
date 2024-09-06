fb2a70fee6012ed30063b9c7e15cd2995a94240b
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-06 14:34:44 +0900

    編集情報保存機能の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_params, only: [:show, :edit]
+  before_action :set_params, only: [:show, :edit, :update]
   def index
     @items = Item.order(created_at: :DESC).includes(:user)
   end
@@ -22,6 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
 
