dc89dac87e2997bf96a88a6ea205b7d92df44238
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-06 14:11:18 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_params, only: [:show, :edit]
   def index
     @items = Item.order(created_at: :DESC).includes(:user)
   end
@@ -18,11 +19,18 @@ class ItemsController < ApplicationController
   end
 
   def show 
-    @item = Item.find(params[:id])
   end
+
+  def edit
+  end
+
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :expense_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @item = Item.find(params[:id])
   end
 end
