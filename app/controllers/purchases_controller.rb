b02a0ce73fa78e215080825f57f6ed4097626cb5
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-06 22:14:25 +0900

    購入記録・配送先情報の保存機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,26 @@
 class PurchasesController < ApplicationController
+  before_action :set_item_params
+
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address =PurchaseAddress.new
   end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def purchase_address_params
+    params.require(:purchase_address).permit(:item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id)
+  end
+
+  def set_item_params
+    @item = Item.find(params[:item_id])
+  end
+
 end
