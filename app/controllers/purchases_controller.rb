c126417c361185ca3a4a36bd0e3a11d0d0a66c2d
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-07 16:42:45 +0900

    クレジット機能のサーバーサイドにおける設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,7 +7,9 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_address_params)
-    if @purchase_address.save
+    if @purchase_address.valid?
+      pay_item
+      @purchase_address.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
@@ -16,11 +18,19 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_address_params
-    params.require(:purchase_address).permit(:item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id)
+    params.require(:purchase_address).permit(:item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id).merge(token: params[:token])
   end
 
   def set_item_params
     @item = Item.find(params[:item_id])
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_2d999fa97b61415174b9600e"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: @purchase_address.token,
+      currency: 'jpy'
+    )
+  end
 end
